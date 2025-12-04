242558049d9e10e7b1d433469d28c44b2d18e839
Maximilian Rödiger <max@Mac.localdomain>
2025-12-04 11:58:45 +0100

    Added constant STAGES with ASCII Art Stages to display. Added function display_game_state to call it from play game.

--- snowman.py
+++ snowman.py
@@ -3,16 +3,61 @@ import random
 # List of secret words
 WORDS = ["python", "git", "github", "snowman", "meltdown"]
 
+# Snowman ASCII Art stages
+STAGES = [
+     # Stage 0: Full snowman
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     ( : ) 
+     """,
+     # Stage 1: Bottom part starts melting
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     ( : ) 
+     """,
+     # Stage 2: Only the head remains
+     """
+      ___  
+     /___\\ 
+     (o o) 
+     """,
+     # Stage 3: Snowman completely melted
+     """
+      ___  
+     /___\\ 
+     """
+ ]
+
 
 def get_random_word():
     """Selects a random word from the list."""
     return WORDS[random.randint(0, len(WORDS) - 1)]
 
 
+def display_game_state(mistakes, secret_word, guessed_letters):
+    """Displays the game state."""
+    print(STAGES[mistakes])
+    print("Word: ", end=" ")
+    for char in secret_word:
+        if char in guessed_letters:
+            print(char, end=" ")
+        else:
+            print("_", end=" ")
+    print()
+
+
 def play_game():
     secret_word = get_random_word()
+    guessed_letters = []
+    mistakes = 0
     print("Welcome to Snowman Meltdown!")
-    print("Secret word selected: " + secret_word)  # for testing, later remove this line
+    #print("Secret word selected: " + secret_word)  # for testing, later remove this line
+    display_game_state(mistakes, secret_word, guessed_letters)
 
     # TODO: Build your game loop here.
     # For now, simply prompt the user once:
