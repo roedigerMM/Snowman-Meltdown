dc9a64dbc32c7590619a1fd93551abb69e6fa73b
Maximilian Rödiger <maxroediger@googlemail.com>
2025-12-05 11:36:36 +0100

    Additional line breaks for better readability

--- snowman.py
+++ snowman.py
@@ -3,9 +3,9 @@ from game_logic import play_game
 def main():
     play_game()
     while True:
-        replay = input("Do you want to play again? (y/n)").lower()
+        replay = input("\nDo you want to play again? (y/n)").lower()
         if replay == "n":
-            print("Thank you for playing The Snowman Meltdown!")
+            print("\nThank you for playing The Snowman Meltdown!")
             break
         elif replay == "y":
             play_game()
