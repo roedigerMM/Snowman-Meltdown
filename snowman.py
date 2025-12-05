405e83f7db6bcdd24ae157f7bdc06d9dc23c018f
Maximilian Rödiger <maxroediger@googlemail.com>
2025-12-05 11:14:05 +0100

    Added a loop inside main, to ask the user if he wants to play again.

--- snowman.py
+++ snowman.py
@@ -2,6 +2,13 @@ from game_logic import play_game
 
 def main():
     play_game()
+    while True:
+        replay = input("Do you want to play again? (y/n)").lower()
+        if replay == "n":
+            print("Thank you for playing The Snowman Meltdown!")
+            break
+        elif replay == "y":
+            play_game()
 
 if __name__ == "__main__":
     main()
